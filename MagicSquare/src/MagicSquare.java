4f74b77861b47b1601b10a63773a48295a46f171
Ethan <tethance6203@gmail.com>
2018-05-22 09:56:04 +0800

    Fourth Order Pandiagonal Magic Squares
    Total:384
    Non-Congruence:48
    Basic:3

--- MagicSquare/src/MagicSquare.java
+++ MagicSquare/src/MagicSquare.java
@@ -6,7 +6,10 @@
 public class MagicSquare {
 
     public static void main(String[] args) {
-        int[] a = {1,2,3,4,5,6,7,8,9};//,10,11,12,13,14,15,16};
+        int[] a = {1,	2,	3,	4,
+5,	6,	7,	8,	
+9,	10,	11,	12,	
+13,	14,	15,	16};
         
         MagicPermu permu = new MagicPermu(a, 0);
         System.out.println(permu.order + ", " + permu.sum);
