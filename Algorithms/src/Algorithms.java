d460bbbc82007b7fb8392f642629cb6ede113605
Ethan <tethance6203@gmail.com>
2018-05-28 19:12:55 +0800

    Matrix
    Method:add, adjoint, cofactor, determinant, diagonal, display, 
    eroAddition, eroMultiplication, eroSwitching, getSize, identity, 
    inverse, lowerTriangular, multiply, scalar, submatrix, transpose
    and upperTriangular.

--- Algorithms/src/Algorithms.java
+++ Algorithms/src/Algorithms.java
@@ -1,37 +1,123 @@
 
 import tw.chuan.ethan.algorithms.Matrix;
 
-
 /**
  *
  * @author Ethan
  */
 public class Algorithms {
-    
+
     public static void main(String[] args) {
+        Algorithms alg = new Algorithms();
+        
+        alg.showMatrixEx();
+    }
+
+    void showMatrixEx(){
         int N = 3;
         int flag = 10;
-        double mat[][] = {{1, 3, 5},
-                       {1, 1, 3},
-                       {1, 1, 1}};
-         Matrix.display(mat, flag);
-         System.out.println("Determinant " + 
-                    "of the matrix is : "+
-             Matrix.determinant(mat, N));
-         
-         
-         
-         System.out.println();
-         double[][] result = Matrix.multiply(
-                 new double[][]{
-                     {1, 0, 0},
-                     {0, 1, 0},
-                     {0, 0, 1}},
-                 mat
-                 );
-         Matrix.display(result, flag);
-         
-         
+        double mat[][] = {{1, 3, 5},{1, 1, 3},{1, 1, 1}};
+        System.out.println("----------------Matrix display----------------");
+        Matrix.display(mat);
+
+        System.out.println("----------------Matrix add----------------");
+        double[][] result1 = Matrix.add(
+                mat,
+                mat
+        );
+        Matrix.display(result1);
+
+        System.out.println("----------------Matrix adjoint----------------");
+        double[][] result2 = new double[N][N];
+        Matrix.adjoint(
+                mat,
+                result2
+        );
+        Matrix.display(result1);
+
+        System.out.println("----------------Matrix determinant----------------");
+        double result3 = Matrix.determinant(mat);
+        System.out.println(result3);
+
+        System.out.println("----------------Matrix submatrix----------------");
+        int p = 1, q = 1;
+        System.out.println("submatrix of (" + p + "," + q + ") is");
+        double[][] result4 = Matrix.submatrix(mat,p,q);
+        if(result4==null)
+            System.out.println("It don't have submatrix.");
+        else
+            Matrix.display(result4);
+
+        System.out.println("----------------Matrix getSize----------------");
+        int[] result5 = Matrix.getSize(mat);
+        System.out.println("row:" + result5[0] + "\ncolumn:" + result5[1]);
+
+        System.out.println("----------------Matrix identify----------------");
+        double[][] result6 = Matrix.identity(N);
+        Matrix.display(result6);
+
+        System.out.println("----------------Matrix inverse----------------");
+        double[][] result7 = new double[N][N];
+        if (Matrix.inverse(mat, result7)) {
+            Matrix.display(result7);
+        } else {
+            System.out.println("It don't have inverse matrix.");
+        }
+
+        System.out.println("----------------Matrix multiply----------------");
+        double[][] result8 = Matrix.multiply(
+                mat,
+                mat
+        );
+        Matrix.display(result8);
+        
+        System.out.println("----------------Matrix scalar----------------");
+        double s = 0.1;
+        System.out.println("multipling scalar of " + s + " is");
+        double[][] result9 = Matrix.scalar(
+                s,
+                mat
+        );
+        Matrix.display(result9, flag);
+        
+        System.out.println("----------------Matrix transpose----------------");
+        double[][] result10 = Matrix.transpose(mat);
+        Matrix.display(result10);
+        
+        System.out.println("----------------Matrix Elementary Row Operation Switching----------------");
+        int r1=0,r2=2;
+        System.out.println("Switching row " + (r1+1) + " and row " + (r2+1));
+        Matrix.eroSwitching(mat, r1, r2);
+        Matrix.display(mat);        
+        Matrix.eroSwitching(mat, r1, r2);
+        
+        System.out.println("----------------Matrix Elementary Row Operation Multiplication----------------");
+        r1=1; s=2;
+        System.out.println("Each element in row " + (r1+1) + " is multiplied by " + s);
+        Matrix.eroMultiplication(mat, r1, s);
+        Matrix.display(mat);        
+        Matrix.eroMultiplication(mat, r1, 1/s);
+        
+        System.out.println("----------------Matrix Elementary Row Operation Addition----------------");
+        r1=1; r2=2; s=2;
+        System.out.println("A row " + (r1+1) + " is replaced by the sum of row " + (r1+1) + " a multiple " + s + " by row " + (r2+1));
+        Matrix.eroAddition(mat, r1, r2, s);
+        Matrix.display(mat);        
+        Matrix.eroAddition(mat, r1, r2, -s);
+        
+        System.out.println("----------------Original Matrix----------------");
+        Matrix.display(mat);
+        
+        System.out.println("----------------Upper Triangular Matrix----------------");
+        double[][] result11 = Matrix.upperTriangular(mat);
+        Matrix.display(result11);
+        
+        System.out.println("----------------Lower Triangular Matrix----------------");
+        double[][] result12 = Matrix.lowerTriangular(mat);
+        Matrix.display(result12);
+        
+        System.out.println("----------------Diagonal Matrix----------------");
+        double[][] result13 = Matrix.diagonal(mat);
+        Matrix.display(result13);
     }
-    
 }
--- Algorithms/src/tw/chuan/ethan/algorithms/Matrix.java
+++ Algorithms/src/tw/chuan/ethan/algorithms/Matrix.java
@@ -6,28 +6,109 @@ package tw.chuan.ethan.algorithms;
  * @author Ethan
  */
 public class Matrix {
-    
-    public static int[] getSize(int[][] mat)
+     
+    // Return matC = matA + matB
+    public static double[][] add(double[][] matA, double[][] matB)
     {
-        int n = mat.length;
-        int m = mat[0].length;
-        return new int[]{n,m};
+        int[] sizeA = getSize(matA);
+        int[] sizeB = getSize(matB);
+        if(sizeA[0]!=sizeB[0] || sizeA[1]!=sizeB[1]) throw new RuntimeException("Illegal matrix dimensions");
+        double[][] matC = new double[sizeA[0]][sizeA[1]];
+        for (int i = 0; i < sizeA[0]; i++)
+            for (int j = 0; j < sizeA[1]; j++)
+                matC[i][j] = matA[i][j] + matB[i][j];
+        return matC;
     }
     
-    public static int[] getSize(double[][] mat)
+    // Function to get adjoint of mat[N][N] in adj[N][N].
+    public static void adjoint(double[][] mat, double[][] adj)
     {
-        int n = mat.length;
-        int m = mat[0].length;
-        return new int[]{n,m};
+        int[] size = getSize(mat);
+        int N;
+        if (((N=size[0]) == 1) && N==size[1]){
+            adj[0][0] = 1;
+            return;
+        }
+        
+        if(N!=size[1]) throw new RuntimeException("Illegal matrix dimensions.");
+
+        for (int i=0; i<N; i++){
+            for (int j=0; j<N; j++){
+                // Get cofactor of mat[i][j]
+                // Interchanging rows and columns to get the
+                // transpose of the cofactor matrix
+                adj[j][i] = cofactor(mat, i, j);
+            }
+        }
     }
     
+    // Return cofactor of mat[p][q].
+    public static double cofactor(double[][] mat, int p, int q){
+        double[][] submatrix = submatrix(mat, p, q);
+        if(submatrix==null) throw new RuntimeException("Illegal matrix dimensions or row index.");
+        // sign of adj[j][i] positive if sum of row
+        // and column indexes is even.
+        int sign = ((p+q)%2==0)? 1: -1;
+        return sign*determinant(submatrix);
+    }
     
-    /* function for displaying the matrix */
+    // Recursive function for finding determinant
+    // of matrix. n is current dimension of mat[][].
+    public static double determinant(double[][] mat)
+    {
+        int[] size = getSize(mat);
+        if(size[0]!=size[1]) throw new RuntimeException("Illegal matrix dimensions.");
+        return determinant(mat, mat.length);
+    }
+    private static double determinant(double[][] mat, int n)
+    {
+        int D = 0; // Initialize result
+     
+        // Base case : if matrix contains single
+        // element
+        if (n == 1)
+            return mat[0][0];
+     
+        // Iterate for each element of first row
+        for (int f = 0; f < n; f++){
+            // Getting Cofactor of mat[0][f] and calculating determinant.
+            D += mat[0][f] * cofactor(mat, 0, f);
+        }
+        return D;
+    }
+    
+    // Return matD that is Diagonal matrix
+    public static double[][] diagonal(double[][] mat){
+        int[] size = getSize(mat);
+        if(size[0]!=size[1]) throw new RuntimeException("Illegal matrix dimensions.");
+        
+        int N = size[0];
+        double[][] matD = new double[N][N];
+        
+        for(int i=0;i<N;i++)
+            matD[i] = mat[i].clone();
+        
+        for(int i=0;i<N-1;i++)
+            for(int j=N-1;j>i;j--)
+                matD[i][j] = 0;
+        for(int i=1;i<N;i++)
+            for(int j=0;j<i;j++)
+                matD[i][j] = 0;  
+        
+        return matD;
+    }
+    
+    // Function for displaying the matrix
     public static void display(double[] x, int flag)
     {   
         for (int i = 0; i < x.length; i++)
                 System.out.print(String.format("%."+flag+"f\t", x[i]));
         System.out.print("\n");
+    }
+    public static void display(double[][] mat)
+    {
+        // default flag = 0
+        display(mat, 0);
     }
     public static void display(double[][] mat, int flag)
     {
@@ -41,97 +122,55 @@ public class Matrix {
         }
     }
     
-    // Function to get cofactor of 
-    // mat[p][q] in temp[][].
-    public static void getCofactor(double[][] mat, 
-             double[][] cofactor, int p, int q, int n)
+    // Elementary Row Operation Addition
+    // specific row r1 replaced by sum of that row r1 and a multiple s of another row r2.
+    public static void eroAddition(double[][] mat, int r1, int r2, double s)
     {
-        int i = 0, j = 0;
-
-        // Looping for each element of 
-        // the matrix
-        for (int row = 0; row < n; row++){
-            for (int col = 0; col < n; col++){
-                
-                // Copying into temporary matrix 
-                // only those element which are 
-                // not in given row and column
-                if (row != p && col != q){
-                    cofactor[i][j++] = mat[row][col];
-                    
-                    // Row is filled, so increase 
-                    // row index and reset col 
-                    //index
-                    if (j == n - 1){
-                        j = 0;
-                        i++;
-                    }
-                }
-            }
-        }
+        if(mat[r1].length!=mat[r2].length) throw new RuntimeException("Illegal row dimensions.");
+        double[] tmpColumn = mat[r2].clone();
+        eroMultiplication(mat, r2, s);
+        for(int i=0;i<tmpColumn.length;i++)
+            mat[r1][i] += mat[r2][i];
+        mat[r2] = tmpColumn;
     }
-    
-    
-    /* Recursive function for finding determinant
-    of matrix. n is current dimension of mat[][]. */
-    public static double determinant(double mat[][], int n)
+    // Elementary Row Operation Multiplication
+    // each element in specific row r is multiplied by a non-zero constant c.
+    public static void eroMultiplication(double[][] mat, int r, double c)
     {
-        int D = 0; // Initialize result
-     
-        // Base case : if matrix contains single
-        // element
-        if (n == 1)
-            return mat[0][0];
-         
-        // To store cofactors
-        double [][] cofactor = new double[n-1][n-1]; 
-         
-        // To store sign multiplier
-        int sign = 1; 
-     
-        // Iterate for each element of first row
-        for (int f = 0; f < n; f++){
-            // Getting Cofactor of mat[0][f]
-            getCofactor(mat, cofactor, 0, f, n);
-            D += sign * mat[0][f] 
-               * determinant(cofactor, n - 1);
-     
-            // terms are to be added with 
-            // alternate sign
-            sign = -sign;
-        }
-     
-        return D;
-    }
-    
-    
-    // Function to get adjoint of A[N][N] in adj[N][N].
-    public static void adjoint(double[][] mat, double[][] adj){
         int[] size = getSize(mat);
-        int N;
-        if (((N=size[0]) == 1) || N!=size[1]){
-            adj[0][0] = 1;
-            return;
-        }
-
-        // temp is used to store cofactors of A[][]
-        int sign;
-        double[][] cofactor = new double[N][N];
-
-        for (int i=0; i<N; i++){
-            for (int j=0; j<N; j++){
-                // Get cofactor of A[i][j]
-                getCofactor(mat, cofactor, i, j, N);
-
-                // sign of adj[j][i] positive if sum of row
-                // and column indexes is even.
-                sign = ((i+j)%2==0)? 1: -1;
-
-                // Interchanging rows and columns to get the
-                // transpose of the cofactor matrix
-                adj[j][i] = (sign)*(determinant(cofactor, N-1));
-            }
-        }
+        for(int i=0;i<size[1];i++)
+            mat[r][i] *= c;
+    }
+    // Elementary Row Operation Switching
+    // a specific row r1 is switched with another row r2.
+    public static void eroSwitching(double[][] mat, int r1, int r2)
+    {
+        double[] tmpColumn = mat[r1].clone();
+        mat[r1] = mat[r2].clone();
+        mat[r2] = tmpColumn;
+    }
+    
+    // Return matrix size. Row:size[0] Column:size[1]
+    public static int[] getSize(int[][] mat)
+    {
+        int n = mat.length;
+        int m = mat[0].length;
+        return new int[]{n,m};
+    }
+    public static int[] getSize(double[][] mat)
+    {
+        int n = mat.length;
+        int m = mat[0].length;
+        return new int[]{n,m};
+    }
+    
+    // Return n-by-n identity matrix I
+    public static double[][] identity(int n)
+    {
+        double[][] I = new double[n][n];
+        for (int i = 0; i < n; i++)
+            I[i][i] = 1;
+        return I;
     }
     
     // Function to calculate and store inverse, returns false if
@@ -164,8 +203,27 @@ public class Matrix {
         return true;
     }
     
-    // return matC = matA * matB
-    public static double[][] multiply(double[][] matA, double[][] matB){
+    // Return matLT that is lower triangular matrix
+    public static double[][] lowerTriangular(double[][] mat){
+        int[] size = getSize(mat);
+        if(size[0]!=size[1]) throw new RuntimeException("Illegal matrix dimensions.");
+        
+        int N = size[0];
+        double[][] matLT = new double[N][N];
+        
+        for(int i=0;i<N;i++)
+            matLT[i] = mat[i].clone();
+        
+        for(int i=0;i<N-1;i++)
+            for(int j=N-1;j>i;j--)
+                matLT[i][j] = 0;
+        
+        return matLT;
+    }
+    
+    // Return matC = matA * matB
+    public static double[][] multiply(double[][] matA, double[][] matB)
+    {
         int[] sizeA = getSize(matA);
         int[] sizeB = getSize(matB);
         if (sizeA[1] != sizeB[0]) throw new RuntimeException("Illegal matrix dimensions.");
@@ -177,8 +235,9 @@ public class Matrix {
         return matC;
     }
     
-    // matrix-vector multiplication (y = matA * x)
-    public static double[] multiply(double[][] matA, double[] x) {
+    // Return y = matA * x. Matrix-vector multiplication 
+    public static double[] multiply(double[][] matA, double[] x)
+    {
         int[] sizeA = getSize(matA);
         if (x.length != sizeA[1]) throw new RuntimeException("Illegal matrix dimensions.");
         double[] y = new double[sizeA[0]];
@@ -189,8 +248,9 @@ public class Matrix {
     }
 
 
-    // vector-matrix multiplication (y = x^T A)
-    public static double[] multiply(double[] x, double[][] matA) {
+    // Return y = x^T * A. Vector-matrix multiplication
+    public static double[] multiply(double[] x, double[][] matA)
+    {
         int[] sizeA = getSize(matA);
         if (x.length != sizeA[0]) throw new RuntimeException("Illegal matrix dimensions.");
         double[] y = new double[sizeA[1]];
@@ -198,5 +258,78 @@ public class Matrix {
             for (int i = 0; i < sizeA[0]; i++)
                 y[j] += matA[i][j] * x[i];
         return y;
+    }
+    
+    // Return matB = s * matA. Scalar-matrix multiplication
+    public static double[][] scalar(double s, double[][] matA)
+    {
+        int[] sizeA = getSize(matA);
+        double[][] matB = new double[sizeA[0]][sizeA[1]];
+        for(int i=0;i<sizeA[0];i++)
+            for(int j=0;j<sizeA[1];j++)
+                matB[i][j] = s * matA[i][j];
+        return matB;
+    }
+    
+    // Return submatrix of mat[p][q].
+    public static double[][] submatrix(double[][] mat, int p, int q)
+    {
+        int[] size = getSize(mat);
+        if(size[0]-1==0 || size[1]-1==0 || p<0 || p>=size[0] || q<0 || q>=size[1])
+            return null;
+        
+        double[][] submatrix = new double[size[0]-1][size[1]-1];
+        
+        int i = 0, j = 0;
+
+        // Looping for each element of the matrix
+        for (int row = 0; row < size[0]; row++){
+            for (int col = 0; col < size[1]; col++){
+                
+                // Copying into temporary matrix 
+                // only those element which are 
+                // not in given row and column
+                if (row != p && col != q){
+                    submatrix[i][j++] = mat[row][col];
+                    
+                    // Row is filled, so increase row index 
+                    // and reset col index
+                    if (j == size[1] - 1){
+                        j = 0;
+                        i++;
+                    }
+                }
+            }
+        }
+        return submatrix;
+    }
+    
+    // Return matB = matA^T
+    public static double[][] transpose(double[][] matA)
+    {
+        int[] sizeA = getSize(matA);
+        double[][] matB = new double[sizeA[0]][sizeA[1]];
+        for (int i = 0; i < sizeA[1]; i++)
+            for (int j = 0; j < sizeA[0]; j++)
+                matB[j][i] = matA[i][j];
+        return matB;
+    }
+    
+    // Return matUT that is upper triangular matrix
+    public static double[][] upperTriangular(double[][] mat){
+        int[] size = getSize(mat);
+        if(size[0]!=size[1]) throw new RuntimeException("Illegal matrix dimensions.");
+        
+        int N = size[0];
+        double[][] matUT = new double[N][N];
+        
+        for(int i=0;i<N;i++)
+            matUT[i] = mat[i].clone();
+        
+        for(int i=1;i<N;i++)
+            for(int j=0;j<i;j++)
+                matUT[i][j] = 0;        
+        
+        return matUT;
     }
 }
